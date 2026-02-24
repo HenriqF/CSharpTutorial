05e81b3b8790fc7546d14fe37462a99cedec1550
Henrique Reinaldi <carlosofalmeida@gmail.com>
2026-02-24 10:53:44 -0300

    Add files via upload

---
+++ 025-Teste-Pessoal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args)
+    {
+
+        if (args.Length <= 1)
+        {
+            Console.WriteLine("sem argumentos.");
+            return;
+        }
+
+        int main_length = args[0].Length;
+        for (uint i = 1; i < args.Length-1; i++)
+        {
+            if (args[i].Length != main_length || args[i] != args[i + 1])
+            {
+                Console.WriteLine("algo diferente existe.");
+                return;
+            }
+        }
+
+        Console.WriteLine("Sao todos iguais.");
+        return;
+        
+    }
+}
+
